36a10bf5789675bfa2667509c76f56d3db400766
Julie M. Jacob <julie@juliejacob.net>
2017-10-02 16:42:59 -0400

    Updated js calls

--- ChoreApp/js/chores.js
+++ ChoreApp/js/chores.js
@@ -8,4 +8,36 @@ function Chore(name, frequency, difficulty, room) {
 	this.completed = false;
     this.pauseChore = false;
     
-};
+}
+
+
+//List of household rooms for dropdown list on addChore
+var room = [
+    "Basement",
+    "Stairs",
+    "Living Room",
+    "First Floor Bathroom",
+    "Kitchen",
+    "Dining Room",
+    "First Floor Hall",
+    "Second Floor Hall",
+    "Second Floor Bathroom",
+    "Master Bedroom",
+    "Bedroom 1",
+    "Bedroom 2",
+    "Bedroom 3",
+    "Bedroom 4",
+    "Office",
+    "Playroom",
+    "Third Floor Bathroom"
+];
+
+//populate dropdown list on addChore form
+
+function roomList() {
+    for (var i = 0; i < room.length; i++){
+        var newRoom = document.createElement("option");
+        newRoom.setAttribute("value", room[i]);
+        document.getElementById("roomOption").appendChild(newRoom);
+    }
+}
